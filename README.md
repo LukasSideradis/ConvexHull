e3b2b76c32937b77472c50958e3ec4c2a209d525
DESKTOP-IINJ2MM\Lukin <lukas.sideradis@gmail.com>
2021-11-24 18:35:14 +0100

    Added code.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ConvexHull
 Implementation of the Convex Hull algorithm from the book "Computational Geometry - Algorithms and Applications".
 
-This repository contains the main.cpp file only. Uses SDL2 as the drawing library.
+This repository contains the main.cpp file only. Uses SDL2 as the drawing library. Example on randomized points provided in the code as well.
---
+++ main.cpp
@@ -0,0 +1,161 @@
+#include "SDL.h"
+#include "SDL_image.h"
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <random>
+#include <time.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<SDL_Event>& GetFrameEvents() {
+	static vector<SDL_Event> frame_events;
+	return frame_events;
+}
+
+void Swap(vector<SDL_Point>* arr, int left, int right) {
+	SDL_Point tmp = arr->at(right);
+	arr->at(right) = arr->at(left);
+	arr->at(left) = tmp;
+}
+
+void QuicksortX(vector<SDL_Point>* arr, int left, int right) {
+	if (left < right) {
+		int boundary = left;
+		for (int i = left + 1; i < right; i++) {
+			if (arr->at(i).x < arr->at(left).x) {
+				Swap(arr, i, ++boundary);
+			}
+		}
+		Swap(arr, left, boundary);
+		QuicksortX(arr, left, boundary);
+		QuicksortX(arr, boundary + 1, right);
+	}
+}
+
+void QuicksortY(vector<SDL_Point>* arr, int left, int right) {
+	if (left < right) {
+		int boundary = left;
+		for (int i = left + 1; i < right; i++) {
+			if ((arr->at(i).x == arr->at(left).x) && (arr->at(i).y < arr->at(left).y)) {
+				Swap(arr, i, ++boundary);
+			}
+		}
+		Swap(arr, left, boundary);
+		QuicksortY(arr, left, boundary);
+		QuicksortY(arr, boundary + 1, right);
+	}
+}
+
+vector<SDL_Point> ConvexHull(vector<SDL_Point> points) {
+	QuicksortX(&points, 0, points.size());
+	QuicksortY(&points, 0, points.size());
+
+	vector<SDL_Point> upper;
+	vector<SDL_Point> lower;
+	// Compute the upper hull
+	upper.emplace_back(points.at(0));
+	upper.emplace_back(points.at(1));
+	for (int i = 2; i < points.size(); i++) {
+		upper.emplace_back(points.at(i));
+		while (upper.size() > 2) {
+			// if the determinant of | x3 - x1 | is negative, then the turn is left  oriented
+			//						 | x2 - x1 | is positive, then the turn is right oriented
+			//									 is zero    , then the 3 points are collinear and the redundant point is removed
+			if ( (upper.at(upper.size() - 1).x - upper.at(upper.size() - 3).x) * (upper.at(upper.size() - 2).y - upper.at(upper.size() - 3).y)
+			   - (upper.at(upper.size() - 1).y - upper.at(upper.size() - 3).y) * (upper.at(upper.size() - 2).x - upper.at(upper.size() - 3).x) <= 0) {
+				upper.erase(upper.end() - 2);
+			}
+			else {
+				break;
+			}
+		}	
+	}
+	// Compute the lower hull
+	lower.emplace_back(points.at(points.size() - 1));
+	lower.emplace_back(points.at(points.size() - 2));
+	for (int i = points.size() - 3; i > -1; i--) {
+		lower.emplace_back(points.at(i));
+		while (lower.size() > 2) {
+			// this method works the same for both (left -> right) and (right -> left)
+			if ((lower.at(lower.size() - 1).x - lower.at(lower.size() - 3).x) * (lower.at(lower.size() - 2).y - lower.at(lower.size() - 3).y)
+				- (lower.at(lower.size() - 1).y - lower.at(lower.size() - 3).y) * (lower.at(lower.size() - 2).x - lower.at(lower.size() - 3).x) <= 0) {
+				lower.erase(lower.end() - 2);
+			}
+			else {
+				break;
+			}
+		}
+	}
+	lower.pop_back();
+	lower.erase(lower.begin());
+
+	vector<SDL_Point> convexHull = upper;
+	convexHull.insert(convexHull.end(), lower.begin(), lower.end());
+	return convexHull;
+}
+
+int main(int argc, char* argv[]) {
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
+	SDL_Window* mainWindow = SDL_CreateWindow("Convex Hull", 40, 40, 800, 800, SDL_WINDOW_SHOWN);
+	SDL_Renderer* mainRenderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	
+	srand(time(NULL));
+	random_device rd;
+	mt19937 generator(rd());
+	uniform_int_distribution<int> distr(50, 750);
+	int random_number_1 = 0;
+	int random_number_2 = 0;
+
+	vector<SDL_Point> points;
+	for (int i = 0; i < 1000; i++) {
+		random_number_1 = distr(generator);
+		random_number_2 = distr(generator);
+		points.emplace_back(SDL_Point{ random_number_1, random_number_2 });
+	}
+
+	vector<SDL_Point> convexHull = ConvexHull(points);
+
+	bool quit = false;
+
+	while (!quit) {
+		SDL_Event e;
+		while (SDL_PollEvent(&e)) {
+			GetFrameEvents().emplace_back(e);
+		}
+
+		for (auto& ev : GetFrameEvents()) {
+			if (ev.type == SDL_KEYDOWN && ev.key.keysym.sym == SDLK_q) {
+				quit = true;
+			}
+		}
+
+		SDL_SetRenderDrawColor(mainRenderer, 0, 0, 0, 255);
+		SDL_RenderClear(mainRenderer);
+
+		SDL_SetRenderDrawColor(mainRenderer, 0, 255, 0, 255);
+		for (auto& p : points) {
+			SDL_RenderDrawPoint(mainRenderer, p.x, p.y);
+		}
+
+		SDL_SetRenderDrawColor(mainRenderer, 0, 0, 255, 255);
+
+		for (int i = 0; i < convexHull.size(); i++) {
+			if (i == convexHull.size() - 1) {
+				SDL_RenderDrawLine(mainRenderer, convexHull.at(i).x, convexHull.at(i).y, convexHull.at(0).x, convexHull.at(0).y);
+			}
+			else {
+				SDL_RenderDrawLine(mainRenderer, convexHull.at(i).x, convexHull.at(i).y, convexHull.at(i + 1).x, convexHull.at(i + 1).y);
+			}
+		}
+
+		SDL_RenderPresent(mainRenderer);
+
+		SDL_Delay(1000.0 / 60.0);
+	}
+
+	return 0;
+}
